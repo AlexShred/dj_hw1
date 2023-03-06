36a9c3cbe473cef365f33050b3447469251badff
ALexShred <mkali2065@gmail.com>
2023-03-06 22:47:22 +0600

    third hw

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from .models import Item, Purchase
 
-# Create your views here.
+
+def list_item(request):
+    item_list = Item.objects.all()
+    context = {
+        'items': item_list
+    }
+    return render(request, 'list_item.html', context)
+
+
+def detail_item(request, id):
+    item = get_object_or_404(Item, id=id)
+    return render(request, 'detail_item.html', {
+        'items': item
+    })
